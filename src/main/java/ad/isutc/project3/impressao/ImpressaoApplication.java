ca4372608be07605f599b020bce9cffa364cd8d6
H3L70N <heltonnguammbe@gmail.com>
2024-11-04 12:28:55 +0200

    commit

---
+++ src/main/java/ad/isutc/project3/impressao/ImpressaoApplication.java
@@ -0,0 +1,13 @@
+package ad.isutc.project3.impressao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpressaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpressaoApplication.class, args);
+	}
+
+}
